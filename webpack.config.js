d64a9c169e40069f7b66dad9cf1153d5a234b9db
m2lovric <m2lovric@gmail.com>
2020-02-02 15:54:43 +0100

    Configuring babel-webpack, dependencies

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+}
