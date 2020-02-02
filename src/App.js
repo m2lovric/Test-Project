4b241cf7fed8e7a3b0453a13b6c6f247f846d6e3
m2lovric <m2lovric@gmail.com>
2020-02-02 16:04:29 +0100

    React ready environment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 console.log('App js is running...');
+
+const App = () => {
+    return(
+        <div>
+            <h3>Testing Environment</h3>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
